e55a70284defd5063cfa183fb21b024ca94a6b88
paulo9428 <paulo9428@naver.com>
2019-02-21 16:14:51 +0900

    19-02-21

---
+++ hadoop/map_reduce.py
@@ -0,0 +1,35 @@
+samples = [
+    (2001, 23),
+    (2002, 7),
+    (2002, -12),
+    (2001, 21),
+    (2003, 20),
+    (2005, 13),
+    (2003, 3),
+    (2005, -2),
+    (2003, 22),
+    (2001, -3),
+]
+
+dic = {}
+a = []
+
+for i in samples:
+    if i[0] in dic.keys():
+        dic[i[0]].append(i[1])
+
+    else:
+        dic[i[0]] = .append(i[1])
+
+print(dic)
+
+
+    
+    
+    
+#     if i[0] in dic.keys()
+    
+#     dic[i[0]]   
+
+# print(dic)
+    
