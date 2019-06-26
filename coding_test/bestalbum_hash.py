58f28c755674ad83b08b6e4e4c8a7eb428f5235d
paulo9428 <paulo9428@naver.com>
2019-06-27 00:10:01 +0900

    2019-06-27

---
+++ coding_test/bestalbum_hash.py
@@ -0,0 +1,45 @@
+def solution(genres, plays):
+    answer = []
+    return answer
+
+genres = ["classic", "pop", "classic", "classic", "pop"]
+plays = [500, 600, 150, 800, 2500]
+
+music_dic = {}
+
+# music_dic["pop"]
+
+
+
+
+for i in range(len(genres)):
+    music_dic[i] = {genres[i]:plays[i]}
+
+print(music_dic)
+print(music_dic.items())
+print(music_dic.keys())
+
+classic_sum = 0
+pop_sum = 0 
+sum_dic = {}
+
+
+
+# for i, g in enumerate(genres):
+#     if g == 'classic':
+#         classic_sum += plays[i]
+#         sum_dic['classic'] = classic_sum
+#     elif g == 'pop':
+#         pop_sum += plays[i]
+#         sum_dic['pop'] = pop_sum
+
+# print(sum_dic)
+
+# sum_dic = sorted(sum_dic, key=lambda k : sum_dic[k], reverse = True)
+
+
+
+
+
+        
+
