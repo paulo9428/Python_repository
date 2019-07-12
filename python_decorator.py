fd2bfbe0f6e61fb51ccadf1047f7de8296cad81b
paulo9428 <paulo9428@naver.com>
2019-07-12 17:50:17 +0900

    2019-07-12

--- python_decorator.py
+++ python_decorator.py
@@ -1,3 +1,27 @@
+# @ 심볼 활용하기 
+ 
+def decorator_function(original_function):
+    def wrapper_function():
+        print("{} 함수가 호출되기 전입니다.".format(original_function.__name__))
+        return original_function()
+    return wrapper_function
+ 
+@decorator_function
+def display_1():
+    print("display_1 함수가 실행됐습니다.")
+ 
+@decorator_function
+def display_2():
+    print("display_2 함수가 실행됐습니다.")
+ 
+# display_1 = decorator_function(display_1) 
+# display_2 = decorator_function(display_2) 
+ 
+display_1() # 변수에 함수형 리턴값 할당 없이 바로 함수 호출이 가능하다. 
+display_2() # 변수에 함수형 리턴값 할당 없이 바로 함수 호출이 가능하다. 
+
+#-----------------------------------------------------------------------------------------
+
 def decorator_function(original_function):
     def wrapper_function(*args, **kwargs):
         print("{} 함수가 호출되기 전입니다.".format(original_function.__name__))
