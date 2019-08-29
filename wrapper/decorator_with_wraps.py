a5e0dc8cbf105db283573f8d4aaa0a1de5ffe67a
paulo9428 <paulo9428@naver.com>
2019-08-29 20:48:52 +0900

    2019-08-29

---
+++ wrapper/decorator_with_wraps.py
@@ -0,0 +1,10 @@
+from functools import wraps
+
+def test_decorator(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        print("Decorated Function")
+        return f(*args, **kwargs)
+    
+    return decorated_function
+
